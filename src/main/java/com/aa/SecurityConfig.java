7cf1f31a93a395cf788713d2aa06826915c7bf3a
Alexandru Alexa <x.alx@yahoo.com>
2021-02-27 15:11:57 -0800

    security api

---
+++ src/main/java/com/aa/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.aa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		@SuppressWarnings("deprecation")
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("aa").password("0000").roles("EMPLOYEE"))
+			.withUser(users.username("alexa").password("0000").roles("EMPLOYEE", "MANAGER"))
+			.withUser(users.username("alexandru").password("0000").roles("EMPLOYEE", "ADMIN"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/api/customers").hasRole("EMPLOYEE")
+		.antMatchers(HttpMethod.GET, "/api/customers/**").hasRole("EMPLOYEE")
+		.antMatchers(HttpMethod.POST, "/api/customers").hasAnyRole("MANAGER", "ADMIN")
+		.antMatchers(HttpMethod.POST, "/api/customers/**").hasAnyRole("MANAGER", "ADMIN")
+		.antMatchers(HttpMethod.PUT, "/api/customers").hasAnyRole("MANAGER", "ADMIN")
+		.antMatchers(HttpMethod.PUT, "/api/customers/**").hasAnyRole("MANAGER", "ADMIN")
+		.antMatchers(HttpMethod.DELETE, "/api/customers/**").hasRole("ADMIN")
+		.and()
+		.httpBasic()
+		.and()
+		.csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+	}
+
+	
+}
---
+++ src/main/java/com/aa/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.aa;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+// prin aceasta clasa activez filtrul de securitate pentru acest api
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
